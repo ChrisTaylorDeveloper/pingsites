a92d068d1665fd70b68fef0aa4e19bdfe678079b
Chris Taylor <chris@christaylordeveloper.co.uk>
2024-03-07 18:52:06 +0100

    Add response size to results and improve sample urls.

--- pingsites.sh
+++ pingsites.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-pingsite () {
-    curl --output /dev/null --connect-timeout 5 --max-time 20 --write-out "%{http_code}" -s -S "$1"
+run_curl () {
+    curl --output /dev/null --connect-timeout 5 --max-time 20 --write-out "%{http_code},%{size_download}" -s -S "$1"
 }
 
-tbl=$(printf "ACTUAL\tEXPECTED\tDEPLOYMENT\tURL")
+tbl=$(printf "ACTUAL\tEXPECTED\tSIZE\tURL\tDEPLOYMENT")
 
 while read -r line; do
     url=$(echo "$line" | cut -d ',' -f 1)
     expected=$(echo "$line" | cut -d ',' -f 2)
     deploy=$(echo "$line" | cut -d ',' -f 4)
-    actual=$(pingsite "$url")
-    tbl+=$'\n'$actual$'\t'$expected$'\t'$deploy$'\t'$url
+    the_write_out=$(run_curl "$url")
+    IFS=',' read -ra frags <<< "$the_write_out"
+    tbl+=$'\n'${frags[0]}$'\t'$expected$'\t'${frags[1]}$'\t'$url$'\t'$deploy
 done
 
 echo "$tbl" | column -t -s $'\t'
